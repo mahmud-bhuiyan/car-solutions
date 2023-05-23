54cb3b79d64424415eff2ef768ae4bdf0213cca0
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-05-24 01:44:12 +0600

    search and sorting added in service section

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,15 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServicesCard from "./ServicesCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [asc, setAsc] = useState(true);
+  const searchRef = useRef(null);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("https://car-solutions-server.vercel.app/services")
+    fetch(
+      `https://car-solutions-server.vercel.app/services?search=${search}&sort=${
+        asc ? "asc" : "desc"
+      }`
+    )
+      // fetch(
+      //   `http://localhost:5000/services?search=${search}&sort=${
+      //     asc ? "asc" : "desc"
+      //   }`
+      // )
       .then((res) => res.json())
       .then((data) => setServices(data))
       .catch((error) => console.log(error));
-  }, []);
+  }, [asc, search]);
+
+  const handleSearch = () => {
+    console.log(searchRef.current.value);
+    setSearch(searchRef.current.value);
+  };
 
   return (
     <div className="mt-6">
@@ -21,6 +38,23 @@ const Services = () => {
           humour, or randomized words which do not look even slightly
           believable.
         </p>
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              type="text"
+              ref={searchRef}
+              onChange={handleSearch}
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+          </div>
+        </div>
+        <button
+          onClick={() => setAsc(!asc)}
+          className="btn btn-sm btn-secondary mt-5"
+        >
+          Price {asc ? "High to Low" : "Low to High"}
+        </button>
       </div>
       <div className="my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {services.map((service) => (
